df543077990e7acc82919adbfcaa36dbfe098577
Andrew Hughes <andrewhughes@Andrews-Laptop.local>
2024-06-23 17:41:52 -0500

    Added comments

--- index.js
+++ index.js
@@ -61,36 +61,36 @@ class Menu { // New Menu class
     }
 
     createCompany() {
-        let name = prompt("Please enter company name: ")
+        let name = prompt("Please enter company name: ") // Accept input and push to Company array
         this.companies.push(new Company(name));
     }
 
     showMainMenuOptions() {
-        return prompt('Welcome to the management application.\n0) Exit\n1) New company\n2) View a company\n3) Delete company\n4) View all companies')
+        return prompt('Welcome to the management application.\n0) Exit\n1) New company\n2) View a company\n3) Delete company\n4) View all companies') // show menu options and accept user input
     }
 
     showCompanyMenuOptions(companyInfo) {
-        return prompt(`0) Back\n1) Add new employee\n2) Delete employee\n\n${companyInfo}`)
+        return prompt(`0) Back\n1) Add new employee\n2) Delete employee\n\n${companyInfo}`) // show  menu options for selected company
     }
 
     viewCompanies() {
         let companiesList = '';
-        for (let index = 0; index < this.companies.length; index++) {
+        for (let index = 0; index < this.companies.length; index++) { // set variable companiesList to empty string, iterate through adding each company name, show resulting list to user
             companiesList += index + ') ' + this.companies[index].name + '\n';
         }
         alert(companiesList);
     }
-    
+
     viewCompany() {
-        let index = parseInt(prompt('Enter the number of the company you want to view: '));
-        if (index > -1 && index <= this.companies.length) {
-            this.selectedCompany = this.companies[index];
+        let index = parseInt(prompt('Enter the number of the company you want to view: ')); // set variable index to input
+        if (index > -1 && index <= this.companies.length) { // if index is valid
+            this.selectedCompany = this.companies[index]; // set selectedCompany to the index input
             let description = 'Company Name: ' + this.selectedCompany.name + '\n';
-            description += ' ' + this.selectedCompany.describe() + '\n';
-            for (let index = 0; index < this.selectedCompany.employees.length; index++) {
+            description += ' ' + this.selectedCompany.describe() + '\n'; // Set description to show the name, and then add description of the company
+            for (let index = 0; index < this.selectedCompany.employees.length; index++) { // Iterate through employees of this company and output each employee after their index
                 description += index + ') ' + this.selectedCompany.employees[index].describe() + '\n';
             }
-            let selection1 = this.showCompanyMenuOptions(description);
+            let selection1 = this.showCompanyMenuOptions(description); // Selection1 variable equal to input from companyMenu
             switch (selection1) {
                 case '1':
                     this.createPerson();
@@ -102,21 +102,21 @@ class Menu { // New Menu class
     }
 
     deleteCompany() {
-        let i = prompt('Enter the number of the company you want to delete: ');
+        let i = prompt('Enter the number of the company you want to delete: '); // Splice the selected index from the array
         if (i > -1 && i < this.companies.length) {
             this.companies.splice(i,1);
         }
     }
 
     createPerson() {
-        let name = prompt ('Enter the name of the new employee: ');
+        let name = prompt ('Enter the name of the new employee: '); // Receive input from user and create people using the info
         let age = prompt ('Enter the age of the new employee: ');
         let position = prompt('Enter the position for the new employee: ');
         this.selectedCompany.addEmployee(new Person(name, age, position));
     }
 
     deletePerson() {
-        let index = prompt('Enter the number of the employee you want to delete: ');
+        let index = prompt('Enter the number of the employee you want to delete: '); // Receive index and delete person from employees array
         if (index > -1 && index < this.selectedCompany.employees.length) {
             this.selectedCompany.employees.splice(index,1);    
         }
@@ -127,4 +127,4 @@ class Menu { // New Menu class
 
 
 let menu = new Menu();
-menu.start();
+menu.start(); // Create menu instance and run with start method
